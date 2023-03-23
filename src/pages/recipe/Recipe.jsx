e42294b143cae0528fce475197e4533cf0bde35c
Bruno Cardozo Pereira <bruno.cardozo.pereira@gmail.com>
2023-03-23 16:03:55 -0300

    feat: Recipe component

--- src/pages/recipe/Recipe.jsx
+++ src/pages/recipe/Recipe.jsx
@@ -1,9 +1,28 @@
+import { useParams } from "react-router-dom";
+
+import {useFetch} from "../../hooks/useFetch.jsx";
+
 import './Recipe.css';
 
 export default function Recipe() {
+    const { id } = useParams();
+    const url = 'http://localhost:3000/recipes/' + id;
+    const  { data: recipe, isPending, error } = useFetch(url);
+
     return (
-        <div>
-            <h1>Recipe</h1>
+        <div className="recipe">
+            { error && <p className="error">{ error }</p> }
+            { isPending && <p className="loading">Loading...</p> }
+            { recipe && (
+                <>
+                    <h2 className="page-title">{recipe.title}</h2>
+                    <p>Takes {recipe.cookingTime} to cook.</p>
+                    <ul>
+                        {recipe.ingredients.map(ingredient => <li key={ingredient}>{ingredient}</li>)}
+                    </ul>
+                    <p className="method">{recipe.method}</p>
+                </>
+            )}
         </div>
     );
 }
