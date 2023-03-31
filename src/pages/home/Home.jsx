f575eac9f7918b10d2b5513bb8a3830e4e9670ba
bruno-c-p <bruno.cardozo.pereira@gmail.com>
2023-03-30 23:54:54 -0300

    feat: fetch recipes collection from firestore

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,17 +1,38 @@
-import { useFetch} from "../../hooks/useFetch.jsx";
+import {useEffect, useState} from "react";
+
+import { collection, getDocs } from "firebase/firestore";
+import {database} from "../../firebase/config.js";
 
 import RecipeList from "../../components/RecipeList/RecipeList.jsx";
 
 import './Home.css';
 
 export default function Home() {
-    const  { data: recipes, isPending, error } = useFetch('http://localhost:3000/recipes');
+    const [data, setData] = useState(null);
+    const [isPending, setIsPending] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setIsPending(true);
+        const recipesRef = collection(database, "recipes");
+        getDocs(recipesRef).then((querySnapshot) => {
+            const recipes = [];
+            querySnapshot.forEach((doc) => {
+                recipes.push({ ...doc.data(), id: doc.id });
+            });
+            setData(recipes);
+            setIsPending(false);
+        }).catch((error) => {
+            setError(error.message);
+            setIsPending(false);
+        });
+    }, []);
 
     return (
         <div className="home">
             { error && <p className="error">{ error }</p> }
             { isPending && <p className="loading">Loading...</p> }
-            { recipes && <RecipeList recipes={recipes} /> }
+            { data && <RecipeList recipes={data} /> }
         </div>
     );
 }
